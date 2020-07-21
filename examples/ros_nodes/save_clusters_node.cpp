adbb21d4cf411141c55d989706428a2b26e93378
furynd <furqanaliyuddien@gmail.com>
2020-07-21 17:06:28 +0700

    update imread, change pointcloud input topic

--- examples/ros_nodes/save_clusters_node.cpp
+++ examples/ros_nodes/save_clusters_node.cpp
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
   ros::init(argc, argv, "dynamics_processing");
   ros::NodeHandle nh;
 
-  string topic_clouds = "/velodyne_points";
+  string topic_clouds = "/rslidar_points";
 
   CloudOdomRosSubscriber subscriber(&nh, *proj_params_ptr, topic_clouds);
 
